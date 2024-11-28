491893a36e2aa7e5c0d1f35232f3df8bec3d297f
Jasharaj <jasharaj38624@outlook.com>
2024-11-28 21:02:42 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar, Hero, Features, Footer } from '../components'
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
