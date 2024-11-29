ff8ace967591a180daa9df0556c796cd0fdf55a1
Jasharaj <jasharaj38624@outlook.com>
2024-11-29 09:48:34 +0530

    Update homepage

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Navbar from "./Navbar";
 import { Hero } from "./Hero";
 import { Features } from "./Features";
 import Footer from "./Footer";
+import Phone from "./Phone";
 
 export {
     Navbar,
     Hero,
     Features,
-    Footer
+    Footer,
+    Phone
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Navbar, Hero, Features, Footer } from '../components'
+import { Navbar, Hero, Features, Footer, Phone } from '../components'
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Phone />
       <Features />
       <Footer />
     </div>
